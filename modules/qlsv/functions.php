64a3a7362a28c9140a5c0a36016a0efdd3b2ce03
hieupt36555 <hieuptph36555@fpt.edu.vn>
2024-10-08 17:57:37 +0700

    list ảnh ra site

--- modules/qlsv/functions.php
+++ modules/qlsv/functions.php
@@ -20,20 +20,25 @@ function nv_demo_list($array_data)
 {
     global $module_name, $lang_module, $lang_global, $module_info, $meta_property, $client_info, $page_config, $global_config;
 
+
+
     $xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_info['module_theme']);
     $xtpl->assign('LANG', $lang_module);
     $xtpl->assign('GLANG', $lang_global);
- 
+
     // Kiểm tra nếu mảng không rỗng
-    if(!empty($array_data)){
+    if (!empty($array_data)) {
+        $stt = 1;
         foreach($array_data as $value){
-            $value['birth']= nv_date( 'd/m/y' ,$value['birth']); //chuyển đổi định dạnh ngày tháng
-            $value['url_detail']= nv_url_rewrite( NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name.'&'. NV_OP_VARIABLE. '=details&id='.$value['id']); //làm đẹp url
+            $value['stt'] = $stt++;
+            $value['birth'] = nv_date('d/m/y', $value['birth']); //chuyển đổi định dạnh ngày tháng
+            $value['url_detail'] = nv_url_rewrite(NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=details&id=' . $value['id']); //làm đẹp url
+            $value['image']= NV_BASE_SITEURL . $value['image'];
             $xtpl->assign('DATA', $value);
             $xtpl->parse('main.loop');
         }
     }
-    
+
     $xtpl->parse('main');
     return $xtpl->text('main');
 }
@@ -48,12 +53,14 @@ function view_product_details($product_id)
     $sql = 'SELECT * FROM ' . NV_PREFIXLANG . '_qlsv WHERE id = ' . intval($product_id);
     $product = $db->query($sql)->fetch();
     if (empty($product)) {
-        return false; 
+        return false;
     }
+    $product['birth'] = nv_date('d/m/y', $product['birth']); 
+    $product['image']= NV_BASE_SITEURL . $product['image'];
 
     $xtpl = new XTemplate('detail.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_file);
     $xtpl->assign('DATA', $product);
-    
+
     $xtpl->parse('main');
     return $xtpl->text('main');
 }
--- themes/default/modules/qlsv/main.tpl
+++ themes/default/modules/qlsv/main.tpl
@@ -8,20 +8,24 @@
         </colgroup>
         <thead>
             <tr class="text-center">
-                <th class="text-nowrap">id</th>
-                <th class="text-nowrap">name</th>
-                <th class="text-nowrap">birth</th>
-                <th class="text-nowrap">address</th>
-                <th class="text-nowrap">option</th>
+            <th class="text-nowrap">STT</th>
+            <th class="text-nowrap">Name</th>
+            <th class="text-nowrap">Birth</th>
+            <th class="text-nowrap">Address</th>
+            <th class="text-nowrap">Email</th>
+            <th class="text-nowrap">Image</th>
+            <th class="text-nowrap">Option</th>
             </tr>
         </thead>
         <tbody>
             <!-- BEGIN: loop -->
              <tr class="text-center">
-                <td class="text-nowrap">{DATA.id}</td>
+                <td class="text-nowrap">{DATA.stt}</td>
                 <td class="text-nowrap">{DATA.name}</td>
                 <td class="text-nowrap">{DATA.birth}</td>
                 <td class="text-nowrap">{DATA.address}</td>
+                <td class="text-nowrap">{DATA.email}</td>
+                <td class="text-nowrap"><img src="{DATA.image}" alt="{DATA.image}" style="width: 100px; height: auto;"></td>
                 <td class="text-nowrap"><a href="{DATA.url_detail}" class="btn btn-success btn-sm"><i class="fa fa-edit"></i> Chi Tiết</a></td>
             </tr>
             <!-- END: loop -->
